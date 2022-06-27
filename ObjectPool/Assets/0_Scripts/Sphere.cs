29067bd5199c3f5243048c0f8b6271c44a4bfc80
Akın <82474005+acupofgreentea@users.noreply.github.com>
2022-06-27 19:57:07 +0300

    Object Pool spawning

--- ObjectPool/Assets/0_Scripts/Sphere.cs
+++ ObjectPool/Assets/0_Scripts/Sphere.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
 
 public class Sphere : MonoBehaviour
 {
     private Renderer rend;
 
+    private Rigidbody rb;
+
     private const string GROUND_TAG = "Ground";
+
+    public Action<Sphere> SphereDisable;
 
     private void Awake()
     {
         rend = GetComponent<Renderer>();
+
+        rb = GetComponent<Rigidbody>();
     }
 
     private void OnEnable()
     {
+        rb.velocity = Vector2.zero;
+
         rend.material.color = new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f));
     }
 
@@ -22,6 +32,6 @@ public class Sphere : MonoBehaviour
     {
         if(!other.CompareTag(GROUND_TAG)) return;
 
-        Destroy(gameObject);
+        SphereDisable?.Invoke(this);
     }
 }
--- ObjectPool/Assets/0_Scripts/SphereSpawner.cs
+++ ObjectPool/Assets/0_Scripts/SphereSpawner.cs
@@ -1,13 +1,81 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class SphereSpawner : MonoBehaviour
 {
     [SerializeField] private Sphere sphere;
 
+    [SerializeField] private float spawnRate = 0.01f;
+
+    [SerializeField] private bool useObjectPool;
+
+    private ObjectPool<Sphere> spherePool;
+
+    private float timer = 0;
+
     private void Awake()
     {
-        
+        spherePool = new ObjectPool<Sphere>(CreatePooledObject, OnTakeFromPool, OnReturnToPool, OnDestroyObject, false, 200, 600);
+    }
+
+    private void Update()
+    {
+        if (useObjectPool)
+        {
+            if (timer <= 0)
+            {
+                //spawn
+                spherePool.Get();
+                timer = spawnRate;
+            }
+            else
+            {
+                timer -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            var sph = Instantiate(sphere, GetSpawnPos(), Quaternion.identity);
+            Destroy(sph, 1f);
+        }
+
+    }
+
+    private Vector3 GetSpawnPos()
+    {
+        return new Vector3(Random.Range(transform.position.x - 10, transform.position.x + 10), transform.position.y, transform.position.z);
+    }
+
+    private Sphere CreatePooledObject()
+    {
+        Sphere instance = Instantiate(sphere, Vector3.zero, Quaternion.identity);
+
+        instance.SphereDisable += ReturnObjectToPool;
+
+        instance.gameObject.SetActive(false);
+
+        return instance;
+    }
+
+    private void ReturnObjectToPool(Sphere instance)
+    {
+        spherePool.Release(instance);
+    }
+
+    private void OnTakeFromPool(Sphere instance)
+    {
+        instance.gameObject.SetActive(true);
+        instance.transform.position = GetSpawnPos();
+    }
+
+    private void OnReturnToPool(Sphere instance)
+    {
+        instance.gameObject.SetActive(false);
+    }
+
+    private void OnDestroyObject(Sphere instance)
+    {
+        Destroy(instance.gameObject);
     }
 }
